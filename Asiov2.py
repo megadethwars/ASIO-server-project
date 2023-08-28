3fe2555f47195d305cd6e6f7e84987b1c6588cde
DESKTOP-FGFDBVD\leone <leonellopezvazquez2@gmail.com>
2023-08-28 13:25:18 -0500

    update ffmpeg commands rtmp

--- Asiov2.py
+++ Asiov2.py
@@ -24,9 +24,24 @@ sd.default.samplerate = 44100
 sd.default.dtype = 'int16'
 sd.default.blocksize = 1024
 
+rtmp_url = "rtmp://localhost/live/STREAMTEST2"
 
+ffmpeg_command_rtmp_image =  [
+        'ffmpeg',
+        '-loop', '1',
+        '-i', 'C:/Users/leone/Music/Battletoads.jpg',  # Ruta de la imagen de fondo
+        '-f',  's16le',             # Formato de audio WAV para FFmpeg
+        '-ac', str(CHANNELS),              # Número de canales
+        '-ar', str(SAMPLE_RATE),
+        '-i', '-',           # Utilizamos una tubería para la entrada de audio
+        '-c:a', 'aac',
+        '-c:v', 'libx264',
+        '-shortest',
+        '-f', 'flv',
+        rtmp_url
+    ]
 
-ffmpeg_command = [
+ffmpeg_command_rtsp = [
     'ffmpeg',
     '-f', 's16le',
     '-ac', str(CHANNELS),
@@ -35,6 +50,45 @@ ffmpeg_command = [
     '-acodec', 'mp3',
     '-f', 'rtsp', rtsp_url
 ]
+
+ffmpeg_command_rtmp = [
+    'ffmpeg',
+    '-f', 's16le',
+    '-ac', str(CHANNELS),
+    '-ar', str(SAMPLE_RATE),
+    '-i', '-',
+    '-acodec', 'mp3',
+    '-f', 'flv',  # Cambiar a formato FLV para RTMP
+    rtmp_url  # URL del servidor RTMP
+]
+
+ffmpeg_command_hls = [
+    'ffmpeg',
+    '-f', 's16le',
+    '-ac', str(CHANNELS),
+    '-ar', str(SAMPLE_RATE),
+    '-i', '-',
+    '-acodec', 'aac',  # Cambiamos a AAC para HLS
+    '-f', 'hls',      # Usamos el formato HLS
+    '-hls_time', '10',  # Duración de los segmentos HLS en segundos
+    '-hls_list_size', '6',  # Tamaño de la lista de reproducción HLS
+    '-hls_segment_type', 'mpegts',  # Tipo de segmento HLS
+    '-hls_flags', 'delete_segments',  # Eliminar segmentos antiguos
+    'http://localhost:80/hls/STREAM_NAME.m3u8'  # Nombre de la lista de reproducción HLS
+]
+
+ffmpeg_command_http_flv = [
+    'ffmpeg',
+    '-f', 's16le',
+    '-ac', str(CHANNELS),
+    '-ar', str(SAMPLE_RATE),
+    '-i', '-',
+    '-acodec', 'aac',
+    '-f', 'flv',  # Usamos el formato FLV
+    'http://localhost:80/live/STREAM_NAME.flv'  # Aquí proporcionamos la URL HTTP-FLV como destino de salida
+]
+
+
 rtsp_url = "rtsp://127.0.0.1:5554/stream2"  # Cambiar por la URL del servidor RTSP
 ffmpeg_process = None
 stop_event = threading.Event()  # Evento para detener el proceso de streaming
@@ -53,15 +107,6 @@ def start_streaming_audio(sample_rate):
     )
     return process
 
-def int16_to_little_endian_bytes(value):
-    return struct.pack('<h', value)
-
-def combine_and_convert_to_bytes(row):
-    combined_value = row[1] * 255 + row[0]
-    print(combined_value)
-    if combined_value > 32767:
-        combined_value -= 65536
-    return struct.pack('<h', combined_value)
 
 def audio_callback(indata, frames, time, status):
     global ffmpeg_process
@@ -74,8 +119,9 @@ def audio_callback(indata, frames, time, status):
 def run_audio_stream():
     global ffmpeg_process
     sample_rate = 44100  # Frecuencia de muestreo del audio
-    #audio_stream = start_streaming_audio(sample_rate)
-    ffmpeg_process = subprocess.Popen(ffmpeg_command, stdin=subprocess.PIPE)
+    
+    ffmpeg_process = subprocess.Popen(ffmpeg_command_rtmp_image, stdin=subprocess.PIPE)
+    #ffmpeg_process = subprocess.Popen(ffmpeg_command_rtmp, stdin=subprocess.PIPE)
     with sd.InputStream(callback=audio_callback, channels=2, samplerate=sample_rate):
         while True:
             pass  # Pausa para permitir la escritura en la tubería
@@ -88,8 +134,7 @@ if __name__ == "__main__":
     audio_thread.start()
 
     try:
-        while True:
-            pass
+        a=1
     except KeyboardInterrupt:
         stop_event.set()
         audio_thread.join()
